c5fa790e62a6a845ede67223181d8863fa552152
Erkan ŞİRİN <erkansirin78@hotmail.com>
2020-09-12 06:22:46 +0300

    First push

---
+++ dataframe_to_kafka.py
@@ -0,0 +1,114 @@
+# coding: utf-8
+import pandas as pd
+from kafka import KafkaProducer
+import time, sys, os
+from pathlib import Path
+from datetime import datetime
+import argparse
+"""
+Example:
+python dataframe_to_kafka.py -i "D:/Datasets/iris.csv" -s "," -rst 0.5 -e "csv" -t iris -b ["cloudera:9092"] -r 3 -shf "True"
+"""
+
+class DataFrameToKafka:
+
+    def __init__(self, input,  sep, row_sleep_time, source_file_extension, bootstrap_servers,
+                 topic, repeat, shuffle):
+        self.input = input
+        self.sep = sep
+        self.row_sleep_time = row_sleep_time
+        self.repeat = repeat
+        self.shuffle = shuffle
+        self.df = self.read_source_file(source_file_extension)
+        self.topic = topic
+        try:
+            self.producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+        except:
+            print("No Broker available")
+
+    # puts all columns into one as string
+    def turn_df_to_str(self, df):
+        x = df.to_string(header=False,
+                         index=False,
+                         index_names=False).split('\n')
+
+        vals = [','.join(ele.split()) for ele in x]
+        return vals
+
+    def read_source_file(self, extension='csv'):
+        if extension == 'csv':
+            if self.shuffle == "True":
+                df = pd.read_csv(self.input, sep=self.sep).sample(frac=1)
+            else:
+                df = pd.read_csv(self.input, sep=self.sep)
+            df = df.dropna()
+
+            # put all cols into value column
+            df['value'] = self.turn_df_to_str(df)
+            return df
+        else:
+            if self.shuffle is "True":
+                df = pd.read_parquet(self.input, 'auto').sample(frac=1)
+            else:
+                df = pd.read_parquet(self.input, 'auto')
+            df = df.dropna()
+            df['value'] = self.turn_df_to_str(df)
+            return df
+
+    # bir dataframe'i kafka
+    def df_to_kafka(self):
+
+        sayac = 0
+        repeat_counter = 0
+        df_size = len(self.df) * self.repeat
+        total_time = self.row_sleep_time * df_size
+        for i in range(0, self.repeat):
+            for index, row in self.df.iterrows():
+                self.producer.send(self.topic, key=str(index).encode(), value=row[-1].encode())
+                self.producer.flush()
+                time.sleep(self.row_sleep_time)
+                sayac = sayac + 1
+                remaining_per = 100 - (100 * (sayac / df_size))
+                remaining_time_secs = (total_time - (self.row_sleep_time * sayac))
+                remaining_time_mins = remaining_time_secs / 60
+                print(str(index) + " - " + str(row[-1]))
+                print("%d/%d processed, %s %.2f will be completed in %.2f mins." % (
+                    sayac, df_size, "%", remaining_per, remaining_time_mins))
+
+            if sayac >= df_size:
+                break
+        self.producer.close()
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", required=False, type=str, default="input/iris.csv",
+                    help="Veri setine ait path giriniz. Varsayılan input/iris.csv")
+    ap.add_argument("-s", "--sep", required=False, type=str, default=",",
+                    help="seperatör giriniz. Varsayılan , Örn: , | ; vb.")
+    ap.add_argument("-rst", "--row_sleep_time", required=False, type=float, default=0.5,
+                    help="Her bir satır için bekleme süresi. Varsayılan 0.5")
+    ap.add_argument("-e", "--source_file_extension", required=False, type=str, default="csv",
+                    help="Kaynak veri setinin uzantısı nedir?. Varsayılan csv")
+    ap.add_argument("-t", "--topic", required=False, type=str, default="test1",
+                    help="Kafka topic. Varsayılan test1")
+    ap.add_argument("-b", "--bootstrap_servers", required=False, type=str, default=["localhost:9092"],
+                    help="kafka bootstraop servers ve portu  python listesi içinde. Varsayılan [localhost:9092]")
+    ap.add_argument("-r", "--repeat", required=False, type=int, default=1,
+                    help="Dataframe'in kaç tur generate edileceği. Varsayılan 1")
+    ap.add_argument("-shf", "--shuffle", required=False, type=str, default=False,
+                    help="Dataframe'in satırları shuflle edilsin mi?. Varsayılan False")
+
+    args = vars(ap.parse_args())
+
+    df_to_kafka = DataFrameToKafka(
+        input=args['input'],
+        sep=args['sep'],
+        row_sleep_time=args['row_sleep_time'],
+        source_file_extension=args['source_file_extension'],
+        topic=args['topic'],
+        bootstrap_servers=args['bootstrap_servers'],
+        repeat=args['repeat'],
+        shuffle=args['shuffle']
+    )
+    df_to_kafka.df_to_kafka()
---
+++ dataframe_to_log.py
@@ -0,0 +1,141 @@
+# coding: utf-8
+import pandas as pd
+import time, sys, os
+from pathlib import Path
+from datetime import datetime
+import argparse
+from random import randrange
+
+"""
+python dataframe_to_log.py --sep "," --input "input/iris.csv" --output "output" --batch_interval 0.1 --batch_size 10 --source_file_extension "csv" --prefix "iris_" --output_header False --output_index True
+"""
+
+
+class DataFrameDataGenerator:
+    def __init__(self, input, output_folder, batch_interval, repeat, shuffle, batch_size, prefix,
+                 sep, source_file_extension, output_header, output_index):
+        self.sep = sep
+        print("self.sep", self.sep)
+        self.input = input
+        print("self.input", self.input)
+        self.df = self.read_source_file(source_file_extension)
+        print("self.df", len(self.df))
+        self.output_folder = output_folder
+        print("self.output_folder", self.output_folder)
+        self.batch_size = batch_size
+        print("self.batch_size", self.batch_size)
+        self.prefix = prefix
+        print("self.prefix", self.prefix)
+        self.batch_interval = batch_interval
+        print("self.batch_interval", self.batch_interval)
+        self.repeat = repeat
+        print("self.repeat", self.repeat)
+        self.shuffle = shuffle
+        print("self.shuffle", self.shuffle)
+        self.output_header = output_header
+        print("self.output_header", self.output_header)
+        self.output_index = True if output_index is True else False
+        print("self.output_index", self.output_index)
+        print("Starting in {} seconds... ".format(self.batch_interval * self.batch_size))
+
+    def read_source_file(self, extension='csv'):
+        if extension == 'csv':
+            return pd.read_csv(self.input, sep=self.sep)
+        else:
+            return pd.read_parquet(self.input, 'auto')
+
+    # write df to disk
+    def df_to_file_as_log(self):
+
+        # reset counter
+        sayac = 0
+
+        # get dataframe size
+        df_size = len(self.df)
+
+        # calculate total streaming time
+        total_time = self.batch_interval * df_size * self.repeat
+
+        time_list_for_each_batch = []
+        repeat_counter = 1
+        total_counter = 1
+        # loop for each row
+        for i in range(0, self.repeat):
+            for i in range(1, df_size + 1):
+
+                # sleep for each row specified in batch_interval argument
+                time.sleep(self.batch_interval)
+                dateTimeObj = datetime.now()
+
+                # collect timestamps for each row
+                time_list_for_each_batch.append(dateTimeObj)
+
+                # if row number mode batch size equals zero write those rows into disk.
+                # do the same if row size equals to dataframe size.
+                if (i % self.batch_size == 0) or (i == df_size):
+                    # get this batch of dataframe to another one
+                    df_batch = self.df.iloc[sayac:i, :].copy()
+
+                    timestr = time.strftime("%Y%m%d-%H%M%S")
+
+                    # add timestaps that are unique for each row
+                    df_batch['event_time'] = time_list_for_each_batch
+
+                    # Empty timestamp list because it must be fill for the next batch
+                    time_list_for_each_batch = []
+
+                    df_batch.to_csv(self.output_folder + "/" + self.prefix + str(timestr), header=False,
+                                    index=self.output_index, index_label='ID', encoding='utf-8')
+                    sayac = i
+                    remaining_per = 100 - (100 * (total_counter / (self.repeat * df_size)))
+                    remaining_time_secs = (total_time - (self.batch_interval * i * repeat_counter))
+                    remaining_time_mins = remaining_time_secs / 60
+                    print("%d/%d processed, %s %.2f will be completed in %.2f mins." % (
+                        total_counter, df_size * self.repeat, "%", remaining_per, remaining_time_mins))
+                    print("repeat_counter", repeat_counter)
+                    print("total_counter", total_counter)
+                total_counter += 1
+            repeat_counter = repeat_counter + 1
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-s", "--sep", required=False, type=str, default=',',
+                    help="seperatör giriniz. Varsayılan , Örn: , | ; vb.")
+    ap.add_argument("-i", "--input", required=False, type=str, default='input/iris.csv',
+                    help="Veri setine ait path giriniz. default: input/iris.csv Örn: /home/murat/iris.csv")
+    ap.add_argument("-o", "--output", required=False, type=str, default='output',
+                    help="Hedef klasör.  Örnek: /home/murat/log_data")
+    ap.add_argument("-b", "--batch_interval", required=False, type=float, default=0.5,
+                    help="Her satırda kaç saniye beklesin float. Örn: 0.5 (Yarım saniye)")
+    ap.add_argument("-z", "--batch_size", required=False, type=int, default=10,
+                    help="Kaç satırda bir dosya oluşturup hedefe yazsın int. Örn: 100")
+    ap.add_argument("-e", "--source_file_extension", required=False, type=str, default='csv',
+                    help="Kaynak veri setinin uzantısı nedir?. Örn: csv")
+    ap.add_argument("-x", "--prefix", required=False, type=str, default='my_df_',
+                    help="Hedefe yazılacak dosya isminin ön eki. Varsayılan my_df_ , Örn: iris_")
+    ap.add_argument("-oh", "--output_header", required=False, type=bool, default=True,
+                    help="Yazarken sütun isimleri/başlık olsun mu?. Varsayılan True")
+    ap.add_argument("-idx", "--output_index", required=False, type=bool, default=False,
+                    help="Yazarken dataframe index bilgisi olsun mu?. Varsayılan False, eğer True ise index key sütunu ile yazılır.")
+    ap.add_argument("-r", "--repeat", required=False, type=int, default=1,
+                    help="Dataframe'in kaç tur generate edileceği. Varsayılan 1")
+    ap.add_argument("-shf", "--shuffle", required=False, type=bool, default=False,
+                    help="Dataframe'in satırları shuflle edilsin mi?. Varsayılan False")
+
+    args = vars(ap.parse_args())
+
+    df_log_generator = DataFrameDataGenerator(
+        input=args['input'],
+        output_folder=args['output'],
+        batch_interval=args['batch_interval'],
+        batch_size=args['batch_size'],
+        prefix=args['prefix'],
+        sep=args['sep'],
+        source_file_extension=args['source_file_extension'],
+        output_header=args['output_header'],
+        output_index=args['output_index'],
+        repeat=args['repeat'],
+        shuffle=args['shuffle']
+    )
+    df_log_generator.df_to_file_as_log()
