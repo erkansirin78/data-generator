d45864d119187c1b0f20436a9b6f5602947ba999
PINAR KILIÇ <89538650+PinarKilic@users.noreply.github.com>
2022-04-13 00:37:51 +0300

    requirements

--- dataframe_to_postgresql.py
+++ dataframe_to_postgresql.py
@@ -6,11 +6,13 @@ from sqlalchemy import create_engine
 
 
 
+
 class DataFrameToPostgreSQL:
 
     def __init__(self, input, host, user, password, database, table, sep, row_sleep_time,
                  source_file_extension,
-                 repeat, shuffle, key_index):
+                 repeat, shuffle
+                 ):
         self.input = input
         print("input: {}".format(self.input))
         self.host = host
@@ -32,8 +34,6 @@ class DataFrameToPostgreSQL:
         self.shuffle = shuffle
         print("shuffle: {}".format(self.shuffle))
         self.df = self.read_source_file(source_file_extension)
-        self.key_index = key_index
-        print("key_index: {}".format(self.key_index))
         self.conn = None
         try:
             print('Connecting to the PostgreSQL...........')
@@ -73,22 +73,14 @@ class DataFrameToPostgreSQL:
         df_size = len(self.df) * self.repeat
         total_time = self.row_sleep_time * df_size
         for i in range(0, self.repeat):
-            for index, row in self.df.iterrows():
-                if self.key_index == 1000:
-                    self.df.to_sql(name=self.table, con=self.conn, if_exists='append', index=False)
-                    # row[-1] corresponds to all columns which already put in one column named value
-                    # If  -k or --key_index not used pandas df index will be sent to kafka as key
-                else:
-                    self.df.to_sql(name=self.table, con=self.conn, if_exists='append', index=False)
-                    # if -k or --key_index used the column spesified in this option will be sent to kafka as key
-                time.sleep(self.row_sleep_time)
-                sayac = sayac + 1
-                remaining_per = 100 - (100 * (sayac / df_size))
-                remaining_time_secs = (total_time - (self.row_sleep_time * sayac))
-                remaining_time_mins = remaining_time_secs / 60
-                print(str(index) + " - " + str(row[-1]))
-                print("%d/%d processed, %s %.2f will be completed in %.2f mins." % (
-                    sayac, df_size, "%", remaining_per, remaining_time_mins))
+            self.df.to_sql(name=self.table, con=self.conn, if_exists='append', index=False)
+            time.sleep(self.row_sleep_time)
+            sayac = sayac + 1
+            remaining_per = 100 - (100 * (sayac / df_size))
+            remaining_time_secs = (total_time - (self.row_sleep_time * sayac))
+            remaining_time_mins = remaining_time_secs / 60
+            print("%d/%d processed, completed." % (
+                df_size, df_size))
 
             if sayac >= df_size:
                 break
@@ -132,9 +124,7 @@ if __name__ == "__main__":
                     help="How many times to repeat dataset. Default: 1")
     ap.add_argument("-shf", "--shuffle", required=False, type=str2bool, default=False,
                     help="Shuffle the rows?. Default: False")
-    ap.add_argument("-k", "--key_index", required=False, type=int, default=1000,
-                    help="Which column will be send as key to kafka? If not used this option, pandas dataframe index will "
-                         "be send. Default: 1000 indicates pandas index will be used.")
+
 
     args = vars(ap.parse_args())
 
@@ -145,7 +135,6 @@ if __name__ == "__main__":
         source_file_extension=args['source_file_extension'],
         repeat=args['repeat'],
         shuffle=args['shuffle'],
-        key_index=args['key_index'],
         host=args['host'],
         user=args['user'],
         database=args['database'],
